cc55390befb83ff65909a3745df54b8d204613c1
vamsi <vamsikrishnagannamaneni@gmail.com>
2025-01-19 21:17:55 +0000

    Moved Clients, Common, Interfaces, Models, and Services into BinanceDashboardAPI

---
+++ Models/DTOs/FuturesAccountBalanceResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs
+{
+    public class FuturesAccountBalanceResponseDto
+    {
+        public decimal Balance { get; set; }
+
+        public DateTime UpdateTime { get; set; }
+    }
+}
