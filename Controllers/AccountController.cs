4b53e68191a7ca5c87c09a613bf5661876550988
vamsi <vamsikrishnagannamaneni@gmail.com>
2025-01-19 16:49:21 +0000

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BinanceDashboardAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("GetBalance")]
+        public IActionResult Balance()
+        {
+            return new JsonResult(new {balance = 6323});
+        }
+    }
+}
